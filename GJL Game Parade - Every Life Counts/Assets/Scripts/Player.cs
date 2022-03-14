9796b9acf4e5fe2831312d80ef5056e3847228ed
khal-bash <86489037+khal-bash@users.noreply.github.com>
2022-03-14 02:33:19 -0400

    2D MOVEMENT
    -Object can now jump (only when on the ground), move left and right ( but is speed capped).

--- GJL Game Parade - Every Life Counts/Assets/Scripts/Player.cs
+++ GJL Game Parade - Every Life Counts/Assets/Scripts/Player.cs
@@ -6,8 +6,11 @@ public class Player : MonoBehaviour
 {
 
     readonly Vector3 startPos = new Vector3(0.75f,9.75f,-5);
-    readonly Vector3 jumpVelocity = new Vector3(0,2);
+    readonly Vector2 jumpVelocity = new Vector2(0,5);
+    readonly Vector2 horizontalAcceleration = new Vector2(0.15f, 0);
+    readonly float horizontalSpeedcap = 5f;
     Rigidbody2D ownRigidBody;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,18 +18,55 @@ public class Player : MonoBehaviour
         Transform parentTransform = GameObject.Find("RefFrame").GetComponent<Transform>();
         transform.position = parentTransform.position + startPos;
         ownRigidBody = GetComponent<Rigidbody2D>();
-        ownRigidBody.gravityScale = 0.1f;
     }
 
-    // FixedUpdate is called once per set time interval
-    void FixedUpdate()
+    // FixedUpdate makes the cube not jump good
+    void Update()
     {
-        if(Input.GetKeyDown("space") == true) {
-            ownRigidBody.velocity = jumpVelocity;
+        if (Input.GetKeyDown("space")) {
+            TryJump();
         }
     }
 
-    void OnCollisionEnter(Collision col) {
-        ownRigidBody.velocity = Vector2.zero;
+    // Otherwise movement would be tied to frame rate
+    void FixedUpdate()
+    {
+        
+        if (Input.GetKey("d"))
+        {
+            MoveRight();
+        }
+
+        if (Input.GetKey("a"))
+        {
+            MoveLeft();
+        }
+    }
+
+    void TryJump()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.down), 0.3f, LayerMask.GetMask("Ground"));
+        if(hit.collider != null)
+        {
+            ownRigidBody.velocity += jumpVelocity;
+        }
+    }
+
+    void MoveRight()
+    {
+        if((ownRigidBody.velocity + horizontalAcceleration).x >= horizontalSpeedcap)
+        {
+            ownRigidBody.velocity.Set(horizontalSpeedcap, ownRigidBody.velocity.y);
+        }
+        else { ownRigidBody.velocity += horizontalAcceleration; }
+    }
+
+    void MoveLeft()
+    {
+        if ((ownRigidBody.velocity - horizontalAcceleration).x <= -horizontalSpeedcap)
+        {
+            ownRigidBody.velocity.Set(-horizontalSpeedcap, ownRigidBody.velocity.y);
+        }
+        else { ownRigidBody.velocity -= horizontalAcceleration; }
     }
 }
