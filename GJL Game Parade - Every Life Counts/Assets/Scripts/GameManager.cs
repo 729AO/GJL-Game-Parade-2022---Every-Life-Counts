b027cb745cf7cd604af0d9198dce87924b2b7d4b
khal-bash <86489037+khal-bash@users.noreply.github.com>
2022-03-19 22:15:21 -0400

    levelFlow

--- GJL Game Parade - Every Life Counts/Assets/Scripts/GameManager.cs
+++ GJL Game Parade - Every Life Counts/Assets/Scripts/GameManager.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
+    List<string> levelOrder;
+
+    private void Start()
+    {
+        levelOrder = new List<string>() {
+            "staircase",
+            "kaylaDrop",
+            "crazyPhysicsShit" 
+        };
+    }
+
     public void EndLevel() {
 
-        
-
+        Scene current_scene = SceneManager.GetActiveScene();
+        string current_scene_name = current_scene.name;
+        int current_number = levelOrder.IndexOf(current_scene_name);
+        SceneManager.LoadScene(levelOrder[current_number + 1]);
     }
 
 }
